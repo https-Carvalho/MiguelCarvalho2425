960bbffb4a1e564124e26ef4dd99f7e5eb6661b9
https-Carvalho <151029607+https-Carvalho@users.noreply.github.com>
2024-11-27 23:13:42 +0000

    geral
    modificacoes gerais

--- perfumes_nicho (1).sql
+++ perfumes_nicho (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 18-Nov-2024 às 23:01
+-- Tempo de geração: 28-Nov-2024 às 00:13
 -- Versão do servidor: 10.4.32-MariaDB
 -- versão do PHP: 8.2.12
 
@@ -38,9 +38,12 @@ CREATE TABLE `imagens_perfume` (
 --
 
 INSERT INTO `imagens_perfume` (`id`, `perfume_id`, `caminho_imagem`) VALUES
-(1, 1, 'images/sospiro_erbapura_1.jpg'),
-(2, 1, 'images/sospiro_erbapura_2.jpg'),
-(3, 1, 'images/sospiro_erbapura_3.jpg');
+(1, 1, 'images/sospiro_erbapura1.jpg'),
+(2, 1, 'images/sospiro_erbapura2.jpg'),
+(3, 1, 'images/sospiro_erbapura3.jpg'),
+(4, 2, 'images/xerjoff_alexandria_ii_1.jpg'),
+(5, 2, 'images/xerjoff_alexandria_ii_2.jpg'),
+(6, 2, 'images/xerjoff_alexandria_ii_3.jpg');
 
 -- --------------------------------------------------------
 
@@ -49,16 +52,21 @@ INSERT INTO `imagens_perfume` (`id`, `perfume_id`, `caminho_imagem`) VALUES
 --
 
 CREATE TABLE `marcas` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(255) NOT NULL
+  `id_marca` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `caminho_imagem` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `marcas`
 --
 
-INSERT INTO `marcas` (`id`, `nome`) VALUES
-(1, 'Sospiro');
+INSERT INTO `marcas` (`id_marca`, `nome`, `caminho_imagem`) VALUES
+(1, 'Sospiro', 'images/sospiro.jpg'),
+(2, 'Xerjoff', 'images/xerjoff.jpg'),
+(3, 'BDK', 'images/BDK.jpg'),
+(4, 'Stephane Humbert Lucas', 'images/StephaneHumbertLucas.jpg'),
+(5, 'Kajal', 'images/kajal.jpg');
 
 -- --------------------------------------------------------
 
@@ -67,19 +75,32 @@ INSERT INTO `marcas` (`id`, `nome`) VALUES
 --
 
 CREATE TABLE `notas_olfativas` (
-  `id` int(11) NOT NULL,
-  `descricao` varchar(255) NOT NULL
+  `id_notes` int(11) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `tipo` enum('topo','coração','base') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `notas_olfativas`
 --
 
-INSERT INTO `notas_olfativas` (`id`, `descricao`) VALUES
-(1, 'Frutado'),
-(2, 'Cítrico'),
-(3, 'Amadeirado'),
-(4, 'Oriental');
+INSERT INTO `notas_olfativas` (`id_notes`, `descricao`, `tipo`) VALUES
+(1, 'Bergamota', 'topo'),
+(2, 'Limão', 'topo'),
+(3, 'Frutas Vermelhas', 'topo'),
+(4, 'Jasmim', 'topo'),
+(5, 'Rosa', 'coração'),
+(6, 'Madeiras Nobres', 'coração'),
+(7, 'Baunilha', 'base'),
+(8, 'Âmbar', 'base'),
+(9, 'Sândalo', 'base'),
+(10, 'Notas Frutadas', 'topo'),
+(11, 'Limão', 'topo'),
+(12, 'Notas Balsâmicas', 'coração'),
+(13, 'Raíz de Orris', 'coração'),
+(14, 'Cedro', 'base'),
+(15, 'Almíscar', 'base'),
+(16, 'Caramelo', 'base');
 
 -- --------------------------------------------------------
 
@@ -88,12 +109,12 @@ INSERT INTO `notas_olfativas` (`id`, `descricao`) VALUES
 --
 
 CREATE TABLE `perfumes` (
-  `id` int(11) NOT NULL,
+  `id_perfume` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `descricao` text NOT NULL,
   `preco` decimal(10,2) NOT NULL,
   `caminho_imagem` varchar(255) NOT NULL,
-  `caminho_imagem_hover` varchar(255) DEFAULT NULL,
+  `caminho_imagem_hover` varchar(255) NOT NULL,
   `id_marca` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -101,8 +122,9 @@ CREATE TABLE `perfumes` (
 -- Extraindo dados da tabela `perfumes`
 --
 
-INSERT INTO `perfumes` (`id`, `nome`, `descricao`, `preco`, `caminho_imagem`, `caminho_imagem_hover`, `id_marca`) VALUES
-(1, 'Erba Pura Magica', 'Uma fragrância mágica e vibrante com notas frutadas e um toque oriental.', 250.00, 'images/sospiro_erbapura.jpg', 'images/sospiro_erbapuramagica_hover.jpg', 1);
+INSERT INTO `perfumes` (`id_perfume`, `nome`, `descricao`, `preco`, `caminho_imagem`, `caminho_imagem_hover`, `id_marca`) VALUES
+(1, 'Sospiro Erba Pura', 'Uma fragrância mágica e vibrante com notas frutadas e orientais.', 230.00, 'images/sospiro_erba_pura.jpg', 'images/sospiro_erba_pura_hover.jpg', 1),
+(2, 'Xerjoff Alexandria II', 'Uma composição clássica com notas orientais e especiadas, ideal para ocasiões especiais.', 340.00, 'images/xerjoff_alexandria_ii.jpg', 'images/xerjoff_alexandria_ii_hover.jpg', 2);
 
 -- --------------------------------------------------------
 
@@ -112,18 +134,28 @@ INSERT INTO `perfumes` (`id`, `nome`, `descricao`, `preco`, `caminho_imagem`, `c
 
 CREATE TABLE `perfume_notas` (
   `id_perfume` int(11) NOT NULL,
-  `id_nota` int(11) NOT NULL
+  `id_notes` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `perfume_notas`
 --
 
-INSERT INTO `perfume_notas` (`id_perfume`, `id_nota`) VALUES
+INSERT INTO `perfume_notas` (`id_perfume`, `id_notes`) VALUES
 (1, 1),
 (1, 2),
-(1, 3),
-(1, 4);
+(1, 4),
+(1, 8),
+(1, 10),
+(1, 12),
+(1, 13),
+(1, 14),
+(1, 15),
+(1, 16),
+(2, 3),
+(2, 5),
+(2, 6),
+(2, 9);
 
 --
 -- Índices para tabelas despejadas
@@ -140,28 +172,27 @@ ALTER TABLE `imagens_perfume`
 -- Índices para tabela `marcas`
 --
 ALTER TABLE `marcas`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nome` (`nome`);
+  ADD PRIMARY KEY (`id_marca`);
 
 --
 -- Índices para tabela `notas_olfativas`
 --
 ALTER TABLE `notas_olfativas`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_notes`);
 
 --
 -- Índices para tabela `perfumes`
 --
 ALTER TABLE `perfumes`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id_perfume`),
   ADD KEY `id_marca` (`id_marca`);
 
 --
 -- Índices para tabela `perfume_notas`
 --
 ALTER TABLE `perfume_notas`
-  ADD PRIMARY KEY (`id_perfume`,`id_nota`),
-  ADD KEY `id_nota` (`id_nota`);
+  ADD PRIMARY KEY (`id_perfume`,`id_notes`),
+  ADD KEY `id_nota` (`id_notes`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -171,25 +202,25 @@ ALTER TABLE `perfume_notas`
 -- AUTO_INCREMENT de tabela `imagens_perfume`
 --
 ALTER TABLE `imagens_perfume`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `marcas`
 --
 ALTER TABLE `marcas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `notas_olfativas`
 --
 ALTER TABLE `notas_olfativas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_notes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de tabela `perfumes`
 --
 ALTER TABLE `perfumes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_perfume` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restrições para despejos de tabelas
@@ -199,20 +230,20 @@ ALTER TABLE `perfumes`
 -- Limitadores para a tabela `imagens_perfume`
 --
 ALTER TABLE `imagens_perfume`
-  ADD CONSTRAINT `imagens_perfume_ibfk_1` FOREIGN KEY (`perfume_id`) REFERENCES `perfumes` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `imagens_perfume_ibfk_1` FOREIGN KEY (`perfume_id`) REFERENCES `perfumes` (`id_perfume`) ON DELETE CASCADE;
 
 --
 -- Limitadores para a tabela `perfumes`
 --
 ALTER TABLE `perfumes`
-  ADD CONSTRAINT `perfumes_ibfk_1` FOREIGN KEY (`id_marca`) REFERENCES `marcas` (`id`);
+  ADD CONSTRAINT `perfumes_ibfk_1` FOREIGN KEY (`id_marca`) REFERENCES `marcas` (`id_marca`) ON DELETE CASCADE;
 
 --
 -- Limitadores para a tabela `perfume_notas`
 --
 ALTER TABLE `perfume_notas`
-  ADD CONSTRAINT `perfume_notas_ibfk_1` FOREIGN KEY (`id_perfume`) REFERENCES `perfumes` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `perfume_notas_ibfk_2` FOREIGN KEY (`id_nota`) REFERENCES `notas_olfativas` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `perfume_notas_ibfk_1` FOREIGN KEY (`id_perfume`) REFERENCES `perfumes` (`id_perfume`) ON DELETE CASCADE,
+  ADD CONSTRAINT `perfume_notas_ibfk_2` FOREIGN KEY (`id_notes`) REFERENCES `notas_olfativas` (`id_notes`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
